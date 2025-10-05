d074a8bb64d989b9c00a6793b5299ae800e2f263
Eberty Neres Rocha <eberty26@gmail.com>
2025-10-05 01:41:11 -0300

    Import and use CTASection component in Hotsite

--- app/hotsite/page.tsx
+++ app/hotsite/page.tsx
@@ -4,7 +4,7 @@ import Hero from '../../components/hotsite/Hero';
 import Speakers from '../../components/hotsite/Speakers';
 import Location from '../../components/hotsite/Location';
 import Schedule from '../../components/hotsite/Schedule';
-import CTASection from '../../components/hotsite/CTASection'; // 1. Importe o novo componente
+import CTASection from '../../components/hotsite/CTASection'; 
 import Sponsors from '../../components/hotsite/Sponsors';
 
 export const metadata = { /* ... */ };
@@ -13,13 +13,13 @@ export default function Hotsite() {
   return (
     <>
       <Navbar />
-      {/* ADICIONADO PADDING NO TOPO PARA EMPURRAR O CONTEÚDO */}
+      
       <main >
         <Hero />
         <Speakers />
         <Location />
         <Schedule />
-        <CTASection /> {/* 2. Use o componente aqui */}
+        <CTASection /> 
         <Sponsors />
       </main>
     </>
